9347972e77e6747ab4ed27f33d399861d4ae57f0
Daniel Sandy <sandyds@mail.uc.edu>
2024-07-16 15:41:36 -0400

    Initial commit

---
+++ src/ListMaker.java
@@ -0,0 +1,26 @@
+
+
+/**
+ *
+ * @author Daniel Sandy sandyds@mail.uc.edu
+ */
+
+public class ListMaker // class ListMaker
+{
+    public static void main(String[] args) // main()
+    {
+        final String Menu = "A - Add  D - Delete  I - Insert  P - Print  Q - Quit";
+        boolean done = false;
+
+        do
+        {
+            // display the menu
+
+            // get a menu choice
+
+            // execute the choice
+
+        }while (!done);
+
+    } // return main()
+} // end class
